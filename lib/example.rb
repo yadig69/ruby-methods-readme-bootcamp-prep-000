cde7f5367ae1af3ac74f95d4790f5e1ca4ce90f1
Gregory Singleton <yadig69design@gmail.com>
2020-07-17 22:48:58 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,5 @@
 # Use this space to code along with the readme
-def
+
 say_hello_world_ten_times
 phrase = "Hello World!"
 puts phrase
@@ -12,4 +12,8 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+
+def greeting
+  puts "Hello World"
 end
