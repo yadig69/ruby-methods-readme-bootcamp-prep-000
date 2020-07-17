e37a4dd769baba6b99c97ad82ea6fee9d00f71ee
Gregory Singleton <yadig69design@gmail.com>
2020-07-17 23:02:01 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,6 @@
 # Use this space to code along with the readme
 
+say_hello_world_ten_times
 phrase = "Hello World!"
 puts phrase
 puts phrase
@@ -11,3 +12,8 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+
+def greeting
+  puts "Hello World"
+end
